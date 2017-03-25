0754eb59cc215b8dad733ae07174719b9b2ecb75
Facundo Casco <fcasco@gmail.com>
2017-03-25 11:26:51 -0300

    usando kow-handlebars para generar el html

--- buche_ws.js
+++ buche_ws.js
@@ -1,8 +1,33 @@
+/* Buche
+*/
+
 const Koa = require('koa');
+const KoaHandlebars = require('koa-handlebars');
+
 const buche_ws = new Koa();
 
-buche_ws.use(ctx => {
-    ctx.body = '_(o.o)_d';
+/*
+const KoaRouter = require('koa-router');
+
+const buche_router = new KoaRouter();
+
+
+buche_router.get('/', function () {
+    yield this.render('root_view', {});
+}
+*/
+
+
+buche_ws.use(KoaHandlebars({
+    defaultLayout: 'main'
+}));
+
+
+buche_ws.use(function *(context) {
+    yield this.render('index', {
+        'title': 'Buche',
+        'message': '_(o.o)_d'
+    });
 });
 
 buche_ws.listen(60606);
