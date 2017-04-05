def95b80adb47f7778ff6ca2057f961aef12bcc4
Facundo Casco <fcasco@gmail.com>
2017-04-04 21:02:54 -0300

    permite acceder a los recursos del servidor en localhost

--- buche_ember/config/environment.js
+++ buche_ember/config/environment.js
@@ -20,6 +20,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+        'content-src': "'self' http://localhost:42024",
     }
   };
 
