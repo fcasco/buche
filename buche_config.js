53736ff47dd5d191d344fe9ea6df83ae231f4727
Facundo Casco <fcasco@gmail.com>
2017-04-08 20:07:03 -0300

    correccion en configuracion de base de datos

--- buche_config.js
+++ buche_config.js
@@ -3,11 +3,11 @@
 let config = {};
 
 config.db_settings = {
-	'user': 'tester',
-	'pass': 'testthis',
+    'user': 'tester',
+    'pass': 'testthis',
     'host': 'ds145800.mlab.com',
     'port': 45800,
-    'db': 'buche'
+    'name': 'buche'
 }
 
 module.exports = config;
